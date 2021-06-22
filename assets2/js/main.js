c8766868b1057d8b967fd5faf99261f8ae24a2eb
Seasy <seasy.huang@gmail.com>
2021-06-22 10:02:32 -0400

    moving over sidebar skeleton (js, css), some media screen stuff, highlight text (selection), scrolly

--- assets2/js/main.js
+++ assets2/js/main.js
@@ -93,6 +93,90 @@
 
 			}
 
+
+			// Sidebar.
+			// 	if ($sidebar.length > 0) {
+			//
+			// 	var $sidebar_a = $sidebar.find('a');
+			//
+			// 	$sidebar_a
+			// 		.addClass('scrolly')
+			// 		.on('click', function() {
+			//
+			// 			var $this = $(this);
+			//
+			// 			// External link? Bail.
+			// 				if ($this.attr('href').charAt(0) != '#')
+			// 					return;
+			//
+			// 			// Deactivate all links.
+			// 				$sidebar_a.removeClass('active');
+			//
+			// 			// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+			// 				$this
+			// 					.addClass('active')
+			// 					.addClass('active-locked');
+			//
+			// 		})
+			// 		.each(function() {
+			//
+			// 			var	$this = $(this),
+			// 				id = $this.attr('href'),
+			// 				$section = $(id);
+			//
+			// 			// No section for this link? Bail.
+			// 				if ($section.length < 1)
+			// 					return;
+			//
+			// 			// Scrollex.
+			// 				$section.scrollex({
+			// 					mode: 'middle',
+			// 					top: '-20vh',
+			// 					bottom: '-20vh',
+			// 					initialize: function() {
+			//
+			// 						// Deactivate section.
+			// 							$section.addClass('inactive');
+			//
+			// 					},
+			// 					enter: function() {
+			//
+			// 						// Activate section.
+			// 							$section.removeClass('inactive');
+			//
+			// 						// No locked links? Deactivate all links and activate this section's one.
+			// 							if ($sidebar_a.filter('.active-locked').length == 0) {
+			//
+			// 								$sidebar_a.removeClass('active');
+			// 								$this.addClass('active');
+			//
+			// 							}
+			//
+			// 						// Otherwise, if this section's link is the one that's locked, unlock it.
+			// 							else if ($this.hasClass('active-locked'))
+			// 								$this.removeClass('active-locked');
+			//
+			// 					}
+			// 				});
+			// 		});
+			// }
+
+			// Scrolly. (for buttons)
+				$('.scrolly').scrolly({
+					speed: 1000,
+					offset: function() {
+
+						// If <=large, >small, and sidebar is present, use its height as the offset.
+							if (breakpoints.active('<=large')
+							&&	!breakpoints.active('<=small')
+							&&	$sidebar.length > 0)
+								return $sidebar.height();
+
+						return 0;
+
+					}
+				});
+
 	// Main Sections: Two.
 
 		// Lightbox gallery.
